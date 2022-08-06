28e32e7def5570641e969233e8870d10bd4651c8
marti <marti@BULI-PRACOVNA>
2022-08-06 15:17:42 +0200

    update about and post detail templates

--- blog_project/blog_app/models.py
+++ blog_project/blog_app/models.py
@@ -17,7 +17,7 @@ class Post(models.Model):
         self.save()
 
     # return list of approved comments
-    def approve_comment(self):
+    def approved_comments(self):
         return self.comments.filter(approved_comment = True)
 
     def get_absolute_url(self):
